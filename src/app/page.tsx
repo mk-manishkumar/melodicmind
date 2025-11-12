56a5912808083691d239b94a214425f6840b1188
Manish Kumar <manish.login01@gmail.com>
2025-11-12 15:26:17 +0530

    Create HeroSection

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,11 @@
+import Footer from "@/components/Footer";
+import HeroSection from "@/components/HeroSection";
 
 export default function Home() {
   return (
-    <></>
+    <main className="min-h-screen bg-black/96 antialiased bg-grid-white/[0.02]">
+      <HeroSection />
+      <Footer/>
+    </main>
   );
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,7 +5,7 @@ import { HoveredLink, Menu, MenuItem } from "./ui/navbar-menu";
 import { cn } from "@/utils/cn";
 import Link from "next/link";
 
-function Navbar({ className }: { className?: string }) {
+const Navbar = ({ className }: { className?: string }) => {
   const [active, setActive] = useState<string | null>(null);
   return (
     <div className={cn("fixed top-10 inset-x-0 max-w-2xl mx-auto z-50", className)}>
@@ -28,6 +28,6 @@ function Navbar({ className }: { className?: string }) {
       </Menu>
     </div>
   );
-}
+};
 
 export default Navbar;
